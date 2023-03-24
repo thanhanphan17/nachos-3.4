172b8e1e69e3ccafb918cf841848f181859ffa56
thanhanphan17 <thanhanphan17@gmail.com>
2023-03-24 18:09:34 +0700

    [CHANGE] change path in clear.py

--- Source/nachos-3.4/code/clear.py
+++ Source/nachos-3.4/code/clear.py
@@ -1,6 +1,14 @@
 import os
 
-dir_path = r'E:\\workspace\\languages\\cpp\\projects\\nachos\\Source\\nachos-3.4\\code'
+dir_path = r'/'
+file_extenstion = '.o' # You can change it based on your need. 
+
+for root, _, files in os.walk(dir_path):
+    for file in files: 
+        if file.endswith(file_extenstion): # for each file in the dir and the sub directories, if the file name ends with the '.exe'
+            os.remove(os.path.join(root, file)) # Just delete it
+
+
 file_extenstion = '.coff' # You can change it based on your need. 
 
 for root, _, files in os.walk(dir_path):
