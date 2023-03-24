68ab9a68714f0463a93cc8598f579de1c57d29b4
thanhanphan17 <thanhanphan17@gmail.com>
2023-03-25 01:06:57 +0700

    [ADD] add bubble sort

--- Source/nachos-3.4/code/test/bbsort.c
+++ Source/nachos-3.4/code/test/bbsort.c
@@ -1,25 +1,78 @@
-// This function is used for sorting data
-
 #include "syscall.h"
 
-int main() {
-    PrintString("\n\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t|      Our Team - Early Bird      +\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t| Name               | ID         |\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t| Phan Thanh An      | 21127003   |\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t| Le Anh Thu         | 21127175   |\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t| Nguyen Hi Huu      | 21127294   |\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t| Nguyen Minh Dat    | 21127592   |\n");
-    PrintString("\t+--------------------+------------+\n");
-    PrintString("\t| Huynh Duc Thien    | 21127693   |\n");
-    PrintString("\t+--------------------+------------+\n");
+#define MAXN (100)
 
+// swap two elements in an array
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// bubble sort function with function pointer argument
+void bubbleSort(int arr[], int n, int (*compare)(int, int)) {
+    int i = 0, j = 0;
+    int swapped = 0;
     
-    Halt();
+    for (i = 0; i < n - 1; ++i) {
+        swapped = 0;
+        
+        for (j = 0; j < n - i - 1; ++j) {
+            if (compare(arr[j], arr[j + 1])) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+// comparison function for ascending order
+int ascending(int a, int b) {
+    return a > b;
+}
+
+// comparison function for descending order
+int descending(int a, int b) {
+    return a < b;
+}
+
+int main() {
+    int n, arr[MAXN + 1], order, temp, i, j;
+    do {
+        PrintString("Enter n (0 <= n <= 100): ");
+        n = ReadInt();
+        if (n < 0 || n > 100) {
+            PrintString("Please try again\n!");
+        }
+    } while (n < 0 || n > 100);
+
+    for (i = 0; i < n; i++) {
+        PrintString("Enter arr[");
+        PrintInt(i);
+        PrintString("]: ");
+
+        arr[i] = ReadInt();
+    }
+
+    PrintString("\n");
+
+    bubbleSort(arr, n, ascending);
+    PrintString("Sorted array ascending: ");
+    for (i = 0; i < n; i++) {
+        PrintInt(arr[i]);
+        PrintChar(' ');
+    }
+
+    PrintString("\n");
+
+    bubbleSort(arr, n, descending);
+    PrintString("Sorted array descending: ");
+    for (i = 0; i < n; i++) {
+        PrintInt(arr[i]);
+        PrintChar(' ');
+    }
 }
