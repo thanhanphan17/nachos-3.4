977d338f7d853861f2604f0381280da0eff06567
thanhanphan17 <thanhanphan17@gmail.com>
2023-03-25 10:53:17 +0700

    [REFORMAT] reformat project

--- Source/nachos-3.4/code/clear.py
+++ Source/nachos-3.4/code/clear.py
@@ -15,4 +15,4 @@ file_extenstion = '.coff' # You can change it based on your need.
 for root, _, files in os.walk(dir_path):
     for file in files: 
         if file.endswith(file_extenstion): # for each file in the dir and the sub directories
-            os.remove(os.path.join(r
+            os.remove(os.path.join)
